54e545fa1bdf871a937ad47199c9c8630cbdc138
Lucie Krátká <lucikratka@email.cz>
2025-04-14 10:10:11 +0200

    sorting

--- sorting.py
+++ sorting.py
@@ -44,6 +44,17 @@ def bubble_sort(number_array):
                 number_array[num_idx], number_array[num_idx + 1] = number_array[num_idx + 1], number_array[num_idx]
     return number_array
 
+def insertion_sort(number_array):
+    n = len(number_array)
+    for i in range(1, n):
+        key = number_array[i]
+        j = i - 1
+        while j >= 0 and number_array[j] > key:
+            number_array[j + 1] = number_array[j]
+            j = j - 1
+        number_array[j + 1] = key
+    return number_array
+
 
 
 
@@ -52,6 +63,7 @@ def main():
     print(data)
     print(selection_sort(data["series_1"]))
     print(bubble_sort(data["series_2"]))
+    print(insertion_sort(data["series_3"]))
     pass
 
 
